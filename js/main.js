e8857d50834fe83015ee1d4ae2495f77f37be613
TarasDutchak <Tarasdutchak90@gmail.com>
2024-06-21 17:43:53 +0300

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -26,7 +26,32 @@ $(document).ready(function () {
         }
     });
 
+    var swiper = new Swiper(".testimsl", {
+       
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
 
+        breakpoints: {
+            640: {
+              slidesPerView: 2,
+              spaceBetween: 20,
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20,
+            },
+            992: {
+                slidesPerView: 3,
+                spaceBetween: 27,
+            },
+          },
+    });
 
 
 
